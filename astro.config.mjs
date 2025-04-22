4197338e7e7298b21fded7f77cd4d2ab0d27b710
Jordan Walker <walker.jlg@gmail.com>
2025-04-22 18:39:34 +0100

    Add css sourcemaps in dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
+import 'dotenv/config'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: process.env.SITE,
+  base: process.env.BASE_URL,
+  vite: {
+    css: {
+      devSourcemap: true,
+    },
+  },
+})
