ab06e003c8fc34d1f3fe2c0d692c489916f89cc0
Jordan Walker <walker.jlg@gmail.com>
2025-05-04 14:52:38 +0100

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
 import 'dotenv/config'
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE,
   base: process.env.BASE_URL,
+
   vite: {
     css: {
       devSourcemap: true,
     },
   },
+
   devToolbar: {
     enabled: false,
   },
+
+  integrations: [sitemap()],
 })
