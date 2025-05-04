805ee53da4e90219b2ce5eee9ac2e58e8ba1c998
Jordan Walker <walker.jlg@gmail.com>
2025-05-04 14:32:24 +0100

    Add og meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,1 @@
 export const SITE_TITLE = 'Maggi Maid'
-export const SITE_DESCRIPTION =
-  'Professional home care and senior support in Bradford, UK. Our experienced team offers gentle, affordable home care and support services for all seniors. Book your consultation today.'
